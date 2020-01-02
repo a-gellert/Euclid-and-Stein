f1474f18ef14422f1b53a29f419808faf4c01608
Александр Геллерт <sever6131@ya.ru>
2020-01-02 21:59:16 +0600

    Добавьте файлы проекта.

---
+++ EuclidAndStein/Classes/Solver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidAndStein.Classes
+{
+    public class Solver
+    {
+        private static int _result;
+        public static int EuclidGCD(int firstN, int secondN)
+        {
+            return Solver.GCD(firstN, secondN);
+        }
+
+        public static int EuclidGCD(int firstN, int secondN, int thirdN)
+        {
+            int[] array = { firstN, secondN, thirdN };
+
+            return Solver.Simpler(array);
+        }
+
+        public static int EuclidGCD(int firstN, int secondN, int thirdN, int fourthN)
+        {
+            int[] array = { firstN, secondN, thirdN, fourthN };
+
+            return Solver.Simpler(array);
+        }
+
+        public static int EuclidGCD(int firstN, int secondN, int thirdN, int fourthN, int fifthN)
+        {
+            int[] array = { firstN, secondN, thirdN, fourthN, fifthN };
+
+            return Solver.Simpler(array);
+        }
+
+        public static int EuclidGCD(params int[] numbers)
+        {
+            return Solver.Simpler(numbers);
+
+        }
+
+        public static int SteinGCD(int firstN, int secondN)
+        {
+            return 0;
+      
+        }
+
+        private static int GCD(int firstN, int secondN)
+        {
+            if (firstN == secondN) { return secondN; }
+
+            while (firstN != 0 && secondN != 0)
+            {
+                if (firstN > secondN)
+                {
+                    firstN %= secondN;
+                }
+                else
+                {
+                    secondN %= firstN;
+                }
+            }
+            return firstN + secondN;
+        }
+
+        private static int Simpler(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                _result = Solver.GCD(numbers[i], numbers[i + 1]);
+            }
+            return _result;
+        }
+    }
+}
---
+++ EuclidAndStein/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EuclidAndStein.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EuclidAndStein
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate(object sender, RoutedEventArgs e)
+        {
+
+            int s = Solver.EuclidGCD(120, 24,32,80,40,20,16,104);
+            textBox1.Text = s.ToString();
+        }
+    }
+}
